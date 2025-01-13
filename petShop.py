6c2ab0ac67c62317ef9d2484c9629a51ebb78e02
Hristo Taushanov <htaushanov@yahoo.com>
2025-01-13 10:53:46 +0200

    Pet Shop

---
+++ petShop.py
@@ -0,0 +1,4 @@
+number_of_dog_food_and_price = int(input())
+number_of_cat_food_and_price = int(input())
+total = float((number_of_dog_food_and_price * 2.50) + (number_of_cat_food_and_price * 4))
+print(f"{total} lv.")
