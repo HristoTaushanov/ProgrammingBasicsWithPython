1320fd9256ba301f320335d1d60a391fbbb22c47
Hristo Taushanov <htaushanov@yahoo.com>
2025-01-13 10:35:41 +0200

    Project Creation task

--- Project Creation.py
+++ Project Creation.py
@@ -1,2 +1,3 @@
 name_of_architect = input()
 number_of_projects = int(input())
+print(f"The architect {name_of_architect} will need {number_of_projects * 3} hours to complete {number_of_projects} project/s.")
