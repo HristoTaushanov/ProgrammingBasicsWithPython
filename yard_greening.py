3a1285084225c82d5c012dbeb22c6c19a504d07f
Hristo Taushanov <htaushanov@yahoo.com>
2025-01-13 11:12:10 +0200

    Yard Greening

---
+++ yard_greening.py
@@ -0,0 +1,6 @@
+square_meters = float(input())
+actual_price = square_meters * 7.61
+discount = actual_price * 0.18
+final_price = actual_price - discount
+print(f"The final price is: {final_price} lv.")
+print(f"The discount is: {discount} lv.")
